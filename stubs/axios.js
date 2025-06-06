e3351fe61845f8616811705d03a4e34a4e06956b
Q <qontact@protonmail.com>
2025-06-06 15:19:47 -0700

    feat: use axios instance with keepAlive

--- stubs/axios.js
+++ stubs/axios.js
@@ -1,3 +1,8 @@
+function stubPost() { //default unmocked post throws to catch stray calls
+  throw new Error('axios.post not stubbed');
+}
+
 module.exports = {
-  post: async () => { throw new Error('axios.post not stubbed'); }
+  post: async () => { stubPost(); },
+  create: () => ({ post: async () => { stubPost(); } }) //mimic axios.create returning instance with post
 };
