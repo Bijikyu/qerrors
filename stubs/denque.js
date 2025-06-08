3e514372a278f75aa3a6c001dffa6a228ddfaedb
Q <qontact@protonmail.com>
2025-06-08 04:54:36 -0700

    use denque queue

---
+++ stubs/denque.js
@@ -0,0 +1,7 @@
+class Denque {
+  constructor(){ this.items = []; }
+  push(val){ this.items.push(val); } //add item to end
+  shift(){ return this.items.shift(); } //remove item from front
+  get length(){ return this.items.length; } //queue size
+}
+module.exports = Denque; //export stub class
