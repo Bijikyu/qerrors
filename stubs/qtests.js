d80b93ed514417ccd2a4dc30bca2d1f159bb51f7
qonductor <qonductor@automated.local>
2025-12-12 16:43:52 +0000

    Preliminary save before implementing Strip All Tests at 2025-12-12 16:43:45

--- stubs/qtests.js
+++
@@ -1,8 +0,0 @@
-// helper stub for temporarily replacing object methods during tests
-module.exports.stubMethod = function(obj, method, impl) {
-  const original = obj[method]; //save original function
-  obj[method] = impl;
-  return function restore() {
-    obj[method] = original; //restore original
-  };
-};
